1a12fe0c163728b1b405c803a036e7e2c38bb7a2
= <=>
2026-06-11 11:44:24 +0100

    addung requirements.txt

--- v2/src/models/baseline.py
+++ v2/src/models/baseline.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import numpy as np
+import joblib
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, RocCurveDisplay
 import matplotlib.pyplot as plt
 
-train_df = pd.read_parquet('../../../datalake/splits/v2/train_df.csv')
+train_df = pd.read_parquet('../../../datalake/splits/v2/train_df.parquet')
 with open ('../../../datalake/clean+features/features.txt','r') as f:
   feature_list = f.read().splitlines()
-train_df['DATETIME'] = pd.to_datetime(train_df['DATETIME'])
-train_df.set_index('DATETIME', inplace=True)
 
 feature_list.remove('ND')
 feature_list.remove('DATETIME')
@@ -25,7 +24,12 @@ print(Y_train.shape)
 model = RandomForestRegressor(n_estimators=500, max_depth=20, random_state=42, verbose = 1)
 model.fit(X_train_scaled, Y_train)
 
-val_df = pd.read_csv('../../../datalake/splits/v2/val_df.csv')
+joblib.dump({"model": model,
+            "scaler": scaler},
+            "baseline_rf.joblib")
+print("Model trained and saved to baseline_rf.joblib")
+
+val_df = pd.read_parquet('../../../datalake/splits/v2/val_df.parquet')
 X_val = val_df[feature_list]
 Y_val = val_df['ND']
 X_val_scaled = scaler.transform(X_val)
@@ -33,4 +37,5 @@ X_val_scaled = scaler.transform(X_val)
 val_predictions = model.predict(X_val_scaled)
 
 nb_disp = RocCurveDisplay.from_estimator(model,X_val_scaled,Y_val)
+plt.savefig("ROC_1.png", dpi = 600)
 plt.show()
